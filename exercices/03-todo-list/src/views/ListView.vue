5fe003883e63c68f4a29414df978061db82d2fe2
macdrien <adrien.bouyssou@gmail.com>
2026-02-23 22:27:44 +0100

    Add base for exercice 03

--- exercices/03-todo-list/src/views/ListView.vue
+++ exercices/03-todo-list/src/views/ListView.vue
@@ -1,44 +1,17 @@
+<script setup lang="js">
+import { ref } from 'vue'
+import TodoList from '@/components/todo/TodoList.vue'
+
+const todos = ref([
+  { id: 1, title: 'Apprendre le VueJS', status: 'In progress' },
+  { id: 2, title: 'Apprendre le ReactJS', status: 'Todo' },
+  { id: 3, title: "Apprendre l'Angular", status: "Won't do" },
+  { id: 4, title: 'Devenir le maître du monde', status: 'Done' },
+])
+</script>
+
 <template>
   <h2>List</h2>
 
-  <ul>
-    <li>
-      <h3>Apprendre le VueJS</h3>
-      <span>In progress</span>
-    </li>
-    <li>
-      <h3>Apprendre le ReactJS</h3>
-      <span>Todo</span>
-    </li>
-    <li>
-      <h3>Apprendre l'Angular</h3>
-      <span>Won't do</span>
-    </li>
-    <li>
-      <h3>Devenir le maître du monde</h3>
-      <span>Done</span>
-    </li>
-  </ul>
+  <TodoList :todos="todos" />
 </template>
-
-<style lang="css" scoped>
-ul {
-  list-style: none;
-  padding: 0;
-  margin: 0 20px;
-}
-
-li {
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 20px;
-  padding: 10px;
-  border-bottom: 1px solid #aaa;
-}
-
-h3 {
-  width: 300px;
-  overflow: hidden;
-}
-</style>
